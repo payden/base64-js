ebff8673ffc19cd95d5fa15acf9b079baff50b9c
Payden Sutherland <payden@paydensutherland.com>
2013-09-24 00:19:25 +0000

    Added decode functionality.

--- base64.js
+++ base64.js
@@ -1,3 +1,17 @@
+/* Author: Payden Sutherland <payden@paydensutherland.com>
+   Feel free to hotlink the version at http://paydensutherland.com/base64.js
+
+
+Usage:
+
+var b = new Base64();
+var b64str = b.encode("data to encode");
+console.log(b64str);
+console.log(b.decode(b64str));
+
+*/
+
+
 Base64 = function() {
   //nothing to see here.
 }
@@ -48,6 +62,43 @@ Base64.prototype = {
     
   },
 
-  decode: function() {
+  decode: function(b64str) {
+    if (b64str.length % 4 != 0) {
+      console.log("String is not a multiple of four, invalid Base64 or no padding.");
+      return false;
+    }
+    var count = b64str.length / 4;
+    var str = "";
+    for (var i = 0; i < count; i++) {
+      var start = i * 4;
+      str += this.decodeQuad(b64str.slice(start, start + 4));
+    }
+    return str;
+  },
+
+  decodeQuad: function(quad) {
+    if (quad.length != 4) {
+      console.log("decodeQuad passed character array length != 4");
+      return false;
+    }
+    var matches = quad.match(/\=/g);
+    if (matches && matches.length == 2) {
+      return String.fromCharCode(this.base64chars.indexOf(quad[0]) << 2 | this.base64chars.indexOf(quad[1]) >> 4);
+    } else if (matches && matches.length == 1) {
+      var num = this.base64chars.indexOf(quad[0]) << 10 |
+                this.base64chars.indexOf(quad[1]) << 4  |
+                this.base64chars.indexOf(quad[2]) >> 2;
+      return String.fromCharCode((num >> 8) & 0xff) + String.fromCharCode(num & 0xff);
+    }
+    var num = this.base64chars.indexOf(quad[0]) << 18 |
+              this.base64chars.indexOf(quad[1]) << 12 |
+              this.base64chars.indexOf(quad[2]) << 6  |
+              this.base64chars.indexOf(quad[3]);
+    var str = Array(3);
+    for (var i = 2; i >=0; i--) {
+      str[i] = String.fromCharCode(num & 0xff);
+      num >>= 8;
+    }
+    return str.join("");
   }
 }
